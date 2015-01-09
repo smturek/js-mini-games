b3224dc1a93cd53a16cfa21c23bc86f8ad94bfd3
smturek <smturek@gmail.com>
2015-01-09 00:07:23 -0700

    added animation

--- platformer/platformer.js
+++ platformer/platformer.js
@@ -1,9 +1,20 @@
 function init() {
   var stage = new createjs.Stage("demoCanvas");
   var circle = new createjs.Shape();
-  circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 50);
-  circle.x = 450;
-  circle.y = 300;
+  var rectangle = new createjs.Shape();
+  circle.graphics.beginFill("Blue").drawCircle(0, 0, 50);
+  rectangle.graphics.beginFill("Green").drawRect(0,400,900, 100);
+  circle.x = 0;
+  circle.y = 350;
   stage.addChild(circle);
-  stage.update();
+  stage.addChild(rectangle);
+
+  createjs.Ticker.addEventListener("tick", tick);
+  createjs.Ticker.setInterval(15);
+
+  function tick() {
+    circle.x = circle.x + 3;
+    if (circle.x > stage.canvas.width + 50) { circle.x = 0; }
+    stage.update();
+  }
 }
