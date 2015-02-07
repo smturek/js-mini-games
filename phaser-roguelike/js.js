8114c7d5f8ec78f419149787175ad6255bacbe19
smturek <smturek@gmail.com>
2015-02-07 15:10:41 -0700

    added player and monster

--- phaser-roguelike/js.js
+++ phaser-roguelike/js.js
@@ -2,16 +2,22 @@ var game = new Phaser.Game(940, 540, Phaser.AUTO, 'foo',
   { preload: preload, create: create, update: update });
 
 var walls;
+var actors;
+var player;
 
 function preload() {
 
   game.load.image('wide', 'assets/wide.png');
   game.load.image('tall', 'assets/tall.png');
+  game.load.image('player', 'assets/player.png');
+  game.load.image('monster', 'assets/monster.png');
 
 }
 
 function create() {
   game.physics.startSystem(Phaser.Physics.ARCADE);
+
+  keys = game.input.keyboard.createCursorKeys();
 
   walls = game.add.group();
 
@@ -26,12 +32,33 @@ function create() {
   walls.getAt(2).immovable = true;
   walls.getAt(3).immovable = true;
 
+  actors = game.add.group()
+  actors.create(300, 300, 'monster');
+  actors.enableBody = true;
+
+  player = game.add.sprite(20, 20, 'player');
+  player.enableBody = true;
 
 
 }
 
 function update() {
 
-
+  if (keys.left.isDown)
+    {
+        player.x -= 5;
+    }
+    else if (keys.right.isDown)
+    {
+        player.x += 5;
+    }
+    else if (keys.up.isDown)
+    {
+        player.y -= 5;
+    }
+    else if (keys.down.isDown)
+    {
+        player.y += 5;
+    }
 
 }
