d025241871f7d005cf5940b3ba164c0d3b886032
smturek <smturek@gmail.com>
2015-02-09 15:13:17 -0700

    added collisions

--- phaser-roguelike/js.js
+++ phaser-roguelike/js.js
@@ -4,6 +4,12 @@ var game = new Phaser.Game(940, 540, Phaser.AUTO, 'foo',
 var walls;
 var actors;
 var player;
+var monster
+
+var fireUp
+var fireDown
+var fireRight
+var fireLeft
 
 function preload() {
 
@@ -16,54 +22,94 @@ function preload() {
 
 function create() {
   game.physics.startSystem(Phaser.Physics.ARCADE);
-
-  keys = game.input.keyboard.createCursorKeys();
+  fireButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 
   walls = game.add.group();
-
-  walls.create(0, 0, 'wide');
-  walls.create(0, 520, 'wide');
-  walls.create(0, 0, 'tall');
-  walls.create(920, 0, 'tall');
-
   walls.enableBody = true;
-  walls.getAt(0).immovable = true;
-  walls.getAt(1).immovable = true;
-  walls.getAt(2).immovable = true;
-  walls.getAt(3).immovable = true;
+
+  var wall = walls.create(0, 0, 'wide');
+  wall.body.immovable = true;
+
+  wall = walls.create(0, 520, 'wide');
+  wall.body.immovable = true;
+
+  wall = walls.create(0, 0, 'tall');
+  wall.body.immovable = true;
+
+  wall = walls.create(920, 0, 'tall');
+  wall.body.immovable = true;
 
   actors = game.add.group()
-  actors.create(300, 300, 'monster');
   actors.enableBody = true;
+
+  monster = actors.create(300, 300, 'monster');
+  monster.body.immovable = true;
 
   player = game.add.sprite(20, 20, 'player');
   game.physics.arcade.enable(player);
-  player.enableBody = true;
-
-  //collisions
-
+  player.body.collideWorldBounds= true;
 
 }
 
 function update() {
-
   game.physics.arcade.collide(player, walls);
+  game.physics.arcade.collide(player, actors)
+
+  keys = game.input.keyboard.createCursorKeys();
+
+  player.body.velocity.y = 0;
+  player.body.velocity.x = 0;
+  monster.body.velocity.y = 0;
+  monster.body.velocity.x = 0;
+
+  if (game.input.keyboard.isDown(87)) {
+    player.body.velocity.y -= 250;
+  }
+  else if (game.input.keyboard.isDown(83)) {
+    player.body.velocity.y += 250;
+  }
+  else if (game.input.keyboard.isDown(68)) {
+    player.body.velocity.x += 250;
+  }
+  else if (game.input.keyboard.isDown(65)) {
+    player.body.velocity.x -= 250;
+  }
 
   if (keys.left.isDown)
     {
-        player.x -= 5;
+      player.body.velocity.x -= 250;
     }
-    else if (keys.right.isDown)
+  else if (keys.right.isDown)
     {
-        player.x += 5;
+      player.body.velocity.x += 250;
     }
-    else if (keys.up.isDown)
+  else if (keys.up.isDown)
     {
-        player.y -= 5;
+      player.body.velocity.y -= 250;
     }
-    else if (keys.down.isDown)
+  else if (keys.down.isDown)
     {
-        player.y += 5;
+      player.body.velocity.y += 250;
+    }
+
+  //radomizes monster movement
+  var monsterMovement = Math.floor(Math.random() * (4 - 0) + 0);
+
+  if (monsterMovement === 0)
+    {
+      monster.body.velocity.x -= 250;
+    }
+  else if (monsterMovement === 1)
+    {
+      monster.body.velocity.x += 250;
+    }
+  else if (monsterMovement === 2)
+    {
+      monster.body.velocity.y -= 250;
+    }
+  else if (monsterMovement === 3)
+    {
+      monster.body.velocity.y += 250;
     }
 
 }
